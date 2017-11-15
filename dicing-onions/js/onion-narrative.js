6699e987570623142cd2bb9342403735252f09c4
Vinod <freakgalaxylol@gmail.com>
2017-11-15 22:13:53 +0530

    Comitting before Be upload

--- dicing-onions/js/onion-narrative.js
+++ dicing-onions/js/onion-narrative.js
@@ -1,19 +1,108 @@
-//$(function() {
-//    $.scrollify({
-//        section : ".scroll-to",
-//        setHeights: false,
-//        scrollSpeed: 500,
-//        overflowScroll: false
-//    });
-//});
+$(function() {
+    $.scrollify({
+        section : ".scroll-to",
+        setHeights: true,
+        scrollSpeed: 2000,
+        overflowScroll: false,
+        offset: 50
+//        ,
+//        before: function(){
+//            $(".scrolly-text").css("color","#ffffff");
+//            $(".body-headings").css("color","#ffffff");
+//        },
+//        after: function(){
+//            $(".scrolly-text").css("color","#9e9e9e");
+//            $(".body-headings").css("color","#212121");
+//        }
+    });
+});
+
+
+
+$(window).resize(function(){
+    screenCheckResize();
+    var location = window.location.href;
+});
+        
+function screenCheckResize(){
+    
+    location = window.location.href;
+    
+    if (($(window).width() < 1400) || ($(window).height() < 800)) {
+        $.scrollify.disable();
+        $(".core").hide();
+        
+        $("#desktop-only").css("display","block");
+        $("#desktop-only").css("position","absolute");
+        $("#desktop-only").css("top","0px");
+        $("#desktop-only").css("left","0px");
+    }
+    else {
+        $.scrollify.enable();
+        $(".core").show();
+        var loc = location.toString().split("#")[0];
+        window.location.href = loc;
+        $("#desktop-only").css("display","none");
+    }
+}
+
+function screenCheck(){
+    if (($(window).width() < 1400) || ($(window).height() < 800)) {
+        $.scrollify.disable();
+        $(".core").hide();
+        
+        $("#desktop-only").css("display","block");
+        $("#desktop-only").css("position","absolute");
+        $("#desktop-only").css("top","0px");
+        $("#desktop-only").css("left","0px");
+    }
+}
+
+
+var idleTime = 0;
+
 
 $(document).ready(
 function(){
+    
+    screenCheck();
+    
+    //Some people might need to know that it's time to scroll ;D
+    $("#scroll-pliss").velocity({"opacity": "0"});
+    
+    var idleInterval = setInterval(timerIncrement, 1000);
+
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+    $(this).scroll(function (e) {
+        idleTime = 0;
+    });
+    
+
+    function timerIncrement() {
+        idleTime = idleTime + 1;
+        if (idleTime > 7) {
+            $("#scroll-pliss").velocity({"opacity": "1"});
+            $("#scroll-down").velocity({"translateY": "30px"}, { loop: 100, easing: "easeInOut", duration: 1000 });
+            $("#scroll-up").velocity({"translateY": "-30px"}, { loop: 100, easing: "easeInOut", duration: 1000 });
+        }
+        else{
+            $("#scroll-pliss").velocity({"opacity": "0"});
+            $("#scroll-down").velocity({"translateY": "0px"});
+            $("#scroll-up").velocity({"translateY": "0px"});
+        }
+    }
+    
     
     for(var i=1; i<13; i++){
         $("#seed"+i).velocity({translateY: "-400px"});
         $("#seed"+i).velocity({"opacity": "0"});
     }
+    
     $("#sunny_1_").velocity({"opacity": "0"});
     $("#sunny_1_").velocity({"translateX": "500px"});
     
@@ -33,8 +122,10 @@ function(){
     
     $("#qty-rise").velocity("stop").velocity({'opacity': '0'});
     $("#qty-highlight").velocity({'opacity': '0'});
+    $("#qty-trend").velocity({'opacity': '0'});
     
     $("#slice-1").velocity("stop").velocity({'opacity': '0'});
+    $("#s1-annote").velocity("stop").velocity({'opacity': '0'});
     $("#s1-lasalgaon").velocity("stop").velocity({'opacity': '0'});
     $("#s1-india").velocity("stop").velocity({'opacity': '0'});
     $("#all-slices").velocity("stop").velocity({'opacity': '0'});
@@ -51,6 +142,8 @@ function(){
     $("#page-3").velocity("stop").velocity({'opacity': '0'});
     $("#p3-reveal1").velocity("stop").velocity({'opacity': '0'});
     $("#p3-reveal2").velocity("stop").velocity({'opacity': '0'});
+    
+    $("#final-img").velocity({"opacity": "0"});
     
 });
 
@@ -86,10 +179,10 @@ var ii1 = new Waypoint({
   element: document.getElementById('instruction-1'),
   handler: function(direction) {
       if (direction === 'down') {
-          $("#onion-cycle").velocity({"opacity": "1"});
+          $("#intro-pic").velocity({"opacity": "1"});
       }
       else{
-          $("#onion-cycle").velocity({"opacity": "0"});
+          $("#intro-pic").velocity({"opacity": "0"});
       }
   },
     offset: globalOffset
@@ -99,6 +192,9 @@ var ii2 = new Waypoint({
   element: document.getElementById('instruction-2'),
   handler: function(direction) {
       if (direction === 'down') {
+          
+          $("#intro-pic").velocity({"opacity": "0"});
+          $("#onion-cycle").velocity({"opacity": "1"});
           
           $("#line1_1_").velocity({"opacity": "1"});
           $("#lbl-oct_1_").velocity({"opacity": "1"});
@@ -134,6 +230,9 @@ var ii2 = new Waypoint({
           
       }
       else{
+          
+          $("#intro-pic").velocity({"opacity": "1"});
+          $("#onion-cycle").velocity({"opacity": "0"});
           
           $("#line1_1_").velocity({"opacity": "0"});
           $("#lbl-oct_1_").velocity({"opacity": "0"});
@@ -192,12 +291,14 @@ var ii5 = new Waypoint({
           $("#fall-dec_1_").velocity({"opacity": "1"});
           
           $("#fall-dec_1_").velocity({"translateY": "-200px"},{delay: 500, duration: 1000});
+          $("#leaves").velocity({"opacity": "0"},{delay: 2000});
           //$("#fall-dec_1_").velocity({"translateY": "-250px"}, { loop: 1, easing: "easeInOut", duration: 2000 });
       }
       else{
           $("#lbl-jan_1_").velocity({"opacity": "0"});
           $("#fall-dec_1_").velocity({"opacity": "0"});
           $("#fall-dec_1_").velocity({"translateY": "0px"});
+          $("#leaves").velocity({"opacity": "1"});
       }
   },
     offset: globalOffset
@@ -255,37 +356,42 @@ var ss2 = new Waypoint({
   element: document.getElementById('slice-2'),
   handler: function(direction) {
       if (direction === 'down') {
-          $("#all-slices").velocity({'opacity': '0'},{delay: 1000});
-          $("#maha-slice").velocity({translateX: "100px"},{delay: 1000});
-          $("#karna-slice").velocity({translateY: "100px"},{delay: 1000});
-          $("#guja-slice").velocity({translateX: "-100px"},{delay: 1000});
-          $("#others-slice").velocity({'opacity': '0'},{delay: 1000});
+          $("#all-slices").velocity({'opacity': '0'});
+          $("#maha-slice_1_").velocity({translateX: "200px",translateY: "-80px"},{delay: 500});
+          $("#karna-slice_1_").velocity({translateX: "120px",translateY: "85px"},{delay: 500});
+          $("#guja-slice_1_").velocity({translateY: "150px"},{delay: 500});
+          $("#others-slice_1_").velocity({translateX: "-180px"},{delay: 500});
+          
+          $("#s1-annote").velocity({'opacity': '1'},{delay:1000});
       }
       else{
-          $("#all-slices").velocity({'opacity': '1'});
-          $("#maha-slice").velocity({translateX: "0px"});
-          $("#karna-slice").velocity({translateY: "0px"});
-          $("#guja-slice").velocity({translateX: "0px"});
-          $("#others-slice").velocity({'opacity': '1'});
+          
+          $("#maha-slice_1_").velocity({translateX: "0px",translateY: "0px"});
+          $("#karna-slice_1_").velocity({translateX: "0px",translateY: "0px"});
+          $("#guja-slice_1_").velocity({translateX: "0px",translateY: "0px"});
+          $("#others-slice_1_").velocity({translateX: "0px",translateY: "0px"});
+          
+          $("#all-slices").velocity({'opacity': '1'},{delay:1000});
+          $("#s1-annote").velocity({'opacity': '0'});
       }
   },
     offset: globalOffset
 });
 
-var ss3 = new Waypoint({
-  element: document.getElementById('slice-3'),
-  handler: function(direction) {
-      if (direction === 'down') {
-          $("#s1-india").velocity({'opacity': '0'});
-          $("#s1-lasalgaon").velocity({'opacity': '1'});
-      }
-      else{
-          $("#s1-india").velocity({'opacity': '1'});
-          $("#s1-lasalgaon").velocity({'opacity': '0'});
-      }
-  },
-    offset: globalOffset
-});
+//var ss3 = new Waypoint({
+//  element: document.getElementById('slice-3'),
+//  handler: function(direction) {
+//      if (direction === 'down') {
+//          $("#s1-india").velocity({'opacity': '0'});
+//          $("#s1-lasalgaon").velocity({'opacity': '1'});
+//      }
+//      else{
+//          $("#s1-india").velocity({'opacity': '1'});
+//          $("#s1-lasalgaon").velocity({'opacity': '0'});
+//      }
+//  },
+//    offset: globalOffset
+//});
 
 //end of slices section
 
@@ -293,14 +399,17 @@ var qty1 = new Waypoint({
   element: document.getElementById('qty-1'),
   handler: function(direction) {
       if (direction === 'down') {
-          $("#s1-lasalgaon").velocity({'opacity': '0'});
+          $("#s1-india").velocity({'opacity': '0'});
           $("#qty-rise").velocity({'opacity': '1'});
           $("#qty-highlight").velocity({'opacity': '1'},{delay: 1000});
+          $('#qty-trend').velocity({'opacity': '1'});
+          
       }
       else{
-          $("#s1-lasalgaon").velocity({'opacity': '1'});
+          $("#s1-india").velocity({'opacity': '1'});
           $("#qty-rise").velocity({'opacity': '0'});
           $("#qty-highlight").velocity({'opacity': '0'});
+          $('#qty-trend').velocity({'opacity': '0'});
       }
   },
     offset: globalOffset
@@ -358,11 +467,9 @@ var wp3 = new Waypoint({
       if (direction === 'down') {
           $("#page-1").velocity({'opacity': '0'});
           $("#page-2").velocity({'opacity': '1'});
-          $("#p2-reveal1").velocity({'opacity': '1'});
       }
       else{
           $("#page-2").velocity({'opacity': '0'});
-          $("#p2-reveal1").velocity({'opacity': '0'});
           $("#page-1").velocity({'opacity': '1'});
       }
   },
@@ -374,10 +481,12 @@ var wp4 = new Waypoint({
   element: document.getElementById('scrolly-6'),
   handler: function(direction) {
       if (direction === 'down') {
-          $("#p2-reveal2").velocity({'opacity': '1'});
+          $("#p2-reveal1").velocity({'opacity': '1'});
+          $("#p2-reveal2").velocity({'opacity': '1'},{'delay':'2000'});
       }
       else{
-          $("#p2-reveal2").velocity({'opacity': '1'});
+          $("#p2-reveal1").velocity({'opacity': '0'});
+          $("#p2-reveal2").velocity({'opacity': '0'});
       }
   },
     offset: globalOffset
@@ -391,8 +500,8 @@ var wp5 = new Waypoint({
           $("#page-2").velocity({'opacity': '0'});
           $("#page-3").velocity({'opacity': '1'});
           
-          $("#p3-reveal1").velocity({'opacity': '1'});
-          $("#p3-reveal2").velocity({'opacity': '1'});
+          $("#p3-reveal1").velocity({'opacity': '1'},{'delay':'2000'});
+          $("#p3-reveal2").velocity({'opacity': '1'},{'delay':'4000'});
       }
       else{
           $("#page-2").velocity({'opacity': '1'});
@@ -407,26 +516,28 @@ var wp5 = new Waypoint({
 
 
 var wp6 = new Waypoint({
-  element: document.getElementById('scrolly-6'),
+  element: document.getElementById('scrolly-8'),
   handler: function(direction) {
       if (direction === 'down') {
-          
+          $("#page-3").velocity({'opacity': '0'});
+          $("#final-img").velocity({"opacity": "1"});
       }
       else{
-          
+          $("#page-3").velocity({'opacity': '1'});
+          $("#final-img").velocity({"opacity": "0"});
       }
   },
     offset: globalOffset
 });
 
 var wp7 = new Waypoint({
-  element: document.getElementById('scrolly-7'),
+  element: document.getElementById('scrolly-9'),
   handler: function(direction) {
       if (direction === 'down') {
-          
+          $("#final-img").velocity({"opacity": "0"});
       }
       else{
-          
+          $("#final-img").velocity({"opacity": "1"});
       }
   },
     offset: globalOffset
