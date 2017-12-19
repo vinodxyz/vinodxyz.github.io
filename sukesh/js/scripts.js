85df5c4608842053cd7b71b49ded0e5c048faa85
Vinod <freakgalaxylol@gmail.com>
2017-12-19 19:20:33 +0530

    Contacts section

---
+++ sukesh/js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(
+function(){
+    $("#linkContact").click(function(){
+        $('html, body').animate({scrollTop: 0}, 800, 'easeOutQuint');
+        return false;
+      });
+});
