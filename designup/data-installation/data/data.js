cb327666a7fad3a971ea740df1cc517882a98023
Vinod <freakgalaxylol@gmail.com>
2018-10-16 20:34:24 +0800

    Overview works
    A version which finally did what we wanted. May have bugs.

--- designup/data-installation/data/data.js
+++ designup/data-installation/data/data.js
@@ -1,16 +1,21 @@
-let dataset =
-{
-    "nodes":[
-        {"name" : "Vinod Kumar", "type" : "user"},
-        {"name" : "chocolates", "type" : "thing"},
-        {"name" : "Prasanta", "type" : "user"},
-        {"name" : "macbook", "type" : "thing"},
-        {"name" : "Rajshree", "type" : "user"}
-    ],
-    "edges":[
-        {"source": 0, "target": 1},
-        {"source": 2, "target": 3},
-        {"source": 4, "target": 1},
-        {"source": 2, "target": 1}
-    ]
-}
+//Web-app hosted at: http://vinodxyz.github.io/designup/data-installation/index.html
+//JSON hosted at: https://api.myjson.com/bins/l1azk
+
+// let dataset =
+// {
+//     "nodes":[
+//         {"name" : "Vinod Kumar", "type" : "user"},
+//         {"name" : "chocolates", "type" : "thing"},
+//         {"name" : "Prasanta", "type" : "user"},
+//         {"name" : "macbook", "type" : "thing"},
+//         {"name" : "Rajshree", "type" : "user"}
+//     ],
+//     "edges":[
+//         {"source": 0, "target": 1},
+//         {"source": 2, "target": 3},
+//         {"source": 4, "target": 1},
+//         {"source": 2, "target": 1}
+//     ]
+// }
+
+
--- designup/data-installation/main.js
+++ designup/data-installation/main.js
@@ -1,231 +1,260 @@
-    
-var width = $(window).width();
-var height = $(window).height();
+var dataset;
+var databackup;
+var jsonID = "11nxww"
 
-var svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-color = d3.scaleOrdinal(d3.schemeCategory10);
-
-var nodes = [];
-var links = [];
-
-for(var i=0; i<dataset.nodes.length; i++){
-    nodes.push(dataset.nodes[i]);
+function getData(){
+    $.ajax({
+        async: false,
+        type: 'GET',
+        url: 'https://api.myjson.com/bins/'+jsonID,
+        success: function(data) {
+            dataset = data;
+            databackup = JSON.parse(JSON.stringify(data));
+        }
+    });
 }
 
-for(var j=0; j<dataset.edges.length; j++){
-    links.push(dataset.edges[j]);
-}
 
-var simulation = d3.forceSimulation(nodes)
-    .force("charge", d3.forceManyBody().strength(-1000))
-    .force("link", d3.forceLink(links).distance(200))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY())
-    .alphaTarget(1)
-    .on("tick", ticked);
+getData();
 
-var g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
-    link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.5).selectAll(".link"),
-    node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
-    label = g.append("g").attr("fill", "black").selectAll(".label");
+        var width = $(window).width();
+        var height = $(window).height();
 
-function dragStarted(d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-}
+        var svg = d3.select("body")
+            .append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-function dragging(d) {
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-}
+        color = d3.scaleOrdinal(d3.schemeCategory10);
 
-function dragEnded(d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-}
+        var nodes = [];
+        var links = [];
 
-loadViz();
+        for(var i=0; i<dataset.nodes.length; i++){
+            nodes.push(dataset.nodes[i]);
+        }
+
+        for(var j=0; j<dataset.edges.length; j++){
+            links.push(dataset.edges[j]);
+        }
+
+        var simulation = d3.forceSimulation(nodes)
+            .force("charge", d3.forceManyBody().strength(-1000))
+            .force("link", d3.forceLink(links).distance(200))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY())
+            .alphaTarget(1)
+            .on("tick", ticked);
+
+        let g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+            link = g.append("g").attr("stroke", "#000").attr("stroke-width", 1.5).selectAll(".link"),
+            node = g.append("g").attr("stroke", "#fff").attr("stroke-width", 1.5).selectAll(".node");
+            label = g.append("g").attr("fill", "black").selectAll(".label");
+
+        function dragStarted(d) {
+            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+        }
+
+        function dragging(d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+        }
+
+        function dragEnded(d) {
+            if (!d3.event.active) simulation.alphaTarget(0);
+            d.fx = null;
+            d.fy = null;
+        }
+
+    runViz();
 
 
 
-function loadViz() {
+function runViz() {
 
-  // Apply the general update pattern to the nodes.
-  node = node.data(nodes, function(d) { return d.name;});
+    // Apply the general update pattern to the nodes.
+    console.log(links);
+    node = node.data(nodes, function(d) { return d.name;});
 
-  node.exit().transition()
-      .attr("r", 0)
-      .remove();
+    node.exit().transition()
+        .attr("r", 0)
+        .remove();
 
-  node = node.enter().append("circle")
-        .attr("fill", function(d) { if(d.type == "user"){ return "blue"} else { return "red"}})
-            .call(function(node) { node.transition().attr("r", 8); })
-            .merge(node)
+    node = node.enter().append("circle")
+            .attr("fill", function(d) { if(d.type == "user"){ return "blue"} else { return "red"}})
+                .call(function(node) { node.transition().attr("r", 8); })
+                .merge(node)
+                .call(d3.drag()
+                .on("start", dragStarted)
+                .on("drag", dragging)
+                .on("end", dragEnded));
+
+        label = label.data(nodes, function(d) { return d.name;});
+
+        label = label
+            .enter().append("text")
+            .attr("class","nodelabel")
+            .text(function(d) { return d.name; })
+            .merge(label)
             .call(d3.drag()
             .on("start", dragStarted)
             .on("drag", dragging)
             .on("end", dragEnded));
 
-    label = label.data(nodes, function(d) { return d.name;});
 
-    label = label
-        .enter().append("text")
-        .attr("class","nodelabel")
-        .text(function(d) { return d.name; })
-        .merge(label)
-        .call(d3.drag()
-        .on("start", dragStarted)
-        .on("drag", dragging)
-        .on("end", dragEnded));
+    // Apply the general update pattern to the links.
+    link = link.data(links, function(d) { return d.source.name + "-" + d.target.name; });
+
+    // Keep the exiting links connected to the moving remaining nodes.
+    link.exit().transition()
+        .attr("stroke-opacity", 0)
+        .attrTween("x1", function(d) { return function() { return d.source.x; }; })
+        .attrTween("x2", function(d) { return function() { return d.target.x; }; })
+        .attrTween("y1", function(d) { return function() { return d.source.y; }; })
+        .attrTween("y2", function(d) { return function() { return d.target.y; }; })
+        .remove();
+
+    link = link.enter().append("line")
+                .call(function(link) { link.transition().attr("stroke-opacity", 1); })
+                .merge(link);
+
+    // Update and restart the simulation.
+    simulation.nodes(nodes);
+    simulation.force("link").links(links);
+    simulation.alpha(1).restart();
+    }
+
+    function ticked() {
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; })
+
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+        label
+        .attr("x", function(d) { return d.x; })
+        .attr("y", function(d) { return d.y+15; });
+    }
 
 
-  // Apply the general update pattern to the links.
-  link = link.data(links, function(d) { return d.source.name + "-" + d.target.name; });
 
-  // Keep the exiting links connected to the moving remaining nodes.
-  link.exit().transition()
-      .attr("stroke-opacity", 0)
-      .attrTween("x1", function(d) { return function() { return d.source.x; }; })
-      .attrTween("x2", function(d) { return function() { return d.target.x; }; })
-      .attrTween("y1", function(d) { return function() { return d.source.y; }; })
-      .attrTween("y2", function(d) { return function() { return d.target.y; }; })
-      .remove();
-
-  link = link.enter().append("line")
-             .call(function(link) { link.transition().attr("stroke-opacity", 1); })
-             .merge(link);
-
-  // Update and restart the simulation.
-  simulation.nodes(nodes);
-  simulation.force("link").links(links);
-  simulation.alpha(1).restart();
-}
-
-function ticked() {
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-
-      label
-      .attr("x", function(d) { return d.x; })
-      .attr("y", function(d) { return d.y+15; });
-}
 
 function updateData(){
+
+    var newNodes = databackup.nodes;
+    var newLinks = databackup.edges;
+
+    var redrawNodes = dataset.nodes;
+    var redrawLinks = dataset.edges;
 
     var userExists = 0;
     var haveExists = 0;
     var needExists = 0;
 
-    var existingUser;
-    var existingHave;
-    var existingNeed;
+    var existingUser, existingUserIndex, userIndex;
+    var existingHave, existingHaveIndex, haveIndex;
+    var existingNeed, existingNeedIndex, needIndex;
 
     var user = {name: $("#txtUsername").val(), type: "user"};
     var have = {name: $("#txtHave").val(), type: "thing" };
     var need = {name: $("#txtNeed").val(), type: "thing" };
     
-    for(var k=0; k<dataset.nodes.length; k++){
-        if(user.name == dataset.nodes[k].name){
+    for(var k=0; k<databackup.nodes.length; k++){
+        if(user.name == databackup.nodes[k].name){
             userExists = 1; 
+            //Changed to dataset 
             existingUser = dataset.nodes[k];
+            existingUserIndex = k;
         }
 
-        if(have.name == dataset.nodes[k].name){
+        if(have.name == databackup.nodes[k].name){
             haveExists = 1;
             existingHave = dataset.nodes[k];
+            existingHaveIndex = k;
         }
 
-        if(need.name == dataset.nodes[k].name){
+        if(need.name == databackup.nodes[k].name){
             needExists = 1;
             existingNeed = dataset.nodes[k];
+            existingNeedIndex = k;
         }
     }
 
-
     if(userExists != 1){
-        nodes.push(user);
+        newNodes.push(user);
+        redrawNodes.push(user);
+        userIndex = newNodes.length-1;
     }
     if(haveExists != 1){
-        nodes.push(have);
+        newNodes.push(have);
+        redrawNodes.push(have);
+        haveIndex = newNodes.length-1;
     }
     if(needExists != 1){
-        nodes.push(need);
+        newNodes.push(need);
+        redrawNodes.push(need);
+        needIndex = newNodes.length-1;
     }
 
     if(existingUser == undefined)
     {
         if(existingHave == undefined)
         {
-            links.push({source: user, target: have});
+            newLinks.push({source: userIndex, target: haveIndex});
+            redrawLinks.push({source: user, target: have});
         }else{
-            links.push({source: user, target: existingHave});
+            links.push({source: userIndex, target: existingHaveIndex});
+            redrawLinks.push({source: user, target: existingHave});
         }
 
         if(existingNeed == undefined)
         {
-            links.push({source: user, target: need});
+            newLinks.push({source: userIndex, target: needIndex});
+            redrawLinks.push({source: user, target: need});
         }else{
-            links.push({source: user, target: existingNeed});
+            newLinks.push({source: userIndex, target: existingNeedIndex});
+            redrawLinks.push({source: user, target: existingNeed});
         }
     }else{
         if(existingHave == undefined)
         {
-            links.push({source: existingUser, target: have});
+            newLinks.push({source: existingUserIndex, target: haveIndex});
+            redrawLinks.push({source: existingUser, target: have});
         }else{
-            links.push({source: existingUser, target: existingHave});
+            newLinks.push({source: existingUserIndex, target: existingHaveIndex});
+            redrawLinks.push({source: existingUser, target: existingHave});
         }
 
         if(existingNeed == undefined)
         {
-            links.push({source: existingUser, target: need});
+            newLinks.push({source: existingUserIndex, target: needIndex});
+            redrawLinks.push({source: existingUser, target: need});
         }else{
-            links.push({source: existingUser, target: existingNeed});
+            newLinks.push({source: existingUserIndex, target: existingNeedIndex});
+            redrawLinks.push({source: existingUser, target: existingNeed});
         }
     }
 
-    dataset.nodes = nodes;
-    dataset.edges = links;
-    loadViz();   
+    databackup.nodes = newNodes;
+    databackup.edges = newLinks;
+
+    nodes = redrawNodes;
+    links = redrawLinks;
+
+    $.ajax({
+        url:"https://api.myjson.com/bins/"+jsonID,
+        type:"PUT",
+        data:JSON.stringify(databackup),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(data, textStatus, jqXHR){
+            runViz(); 
+        }
+    });
+
+      
 }
-
-
-
-
-function onInitFs(fs) {
-
-    fs.root.getFile('log.txt', {create: true}, function(fileEntry) {
-  
-      // Create a FileWriter object for our FileEntry (log.txt).
-      fileEntry.createWriter(function(fileWriter) {
-  
-        fileWriter.onwriteend = function(e) {
-          console.log('Write completed.');
-        };
-  
-        fileWriter.onerror = function(e) {
-          console.log('Write failed: ' + e.toString());
-        };
-  
-        // Create a new Blob and write it to log.txt.
-        var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
-  
-        fileWriter.write(blob);
-  
-      }, errorHandler);
-  
-    }, errorHandler);
-  
-  }
-  
-  window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
